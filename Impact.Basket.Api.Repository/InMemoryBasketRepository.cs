63d8b412f0c033c917e7d0a38ee0aead724ffa10
Diogo <diogo.santos-carvalho@rabobank.com>
2023-07-17 11:51:49 +0200

    implement repository methods

--- Impact.Basket.Api.Repository/InMemoryBasketRepository.cs
+++ Impact.Basket.Api.Repository/InMemoryBasketRepository.cs
@@ -2,6 +2,7 @@
 using Impact.Basket.Api.Domain.Model.Filters;
 using Impact.Basket.Api.Domain.Repositories.Contracts;
 using Impact.Basket.Api.Repository.Dtos;
+using Impact.Basket.Api.Repository.Mappers;
 using Microsoft.Extensions.Logging;
 using System.Collections.Concurrent;
 
@@ -17,34 +18,82 @@ namespace Impact.Basket.Api.Repository
             _logger = logger;
         }
 
-        public Task<Result<Guid>> Create(Domain.Models.Basket entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Result> Delete(Guid id)
-        {
-            throw new NotImplementedException();
-        }
-
         public Task<Result<IEnumerable<Domain.Models.Basket>>> GetAll(PaginationFilter paginationFilter = null)
         {
-            throw new NotImplementedException();
+            if (paginationFilter is null)
+            {
+                _logger.LogDebug("Retrieving all Basket at once");
+                return Task.FromResult(Result.Success(this.Baskets.Values.ToDomain()));
+            }
+
+            _logger.LogDebug("Retrieving paged baskets with page number {pageNumber} and page size of {pageSize}", paginationFilter.PageNumber, paginationFilter.PageSize);
+
+            return Task.FromResult(Result.Success(this.Baskets.Values.Skip((paginationFilter.PageSize - 1 * paginationFilter.PageNumber)).Take(paginationFilter.PageSize).ToDomain()));
         }
 
-        public Task<Result<IEnumerable<Domain.Models.Basket>>> GetByFilter(Func<Domain.Models.Basket, bool> predicate)
+        public Task<Result<Guid>> Create(Domain.Models.Basket basket)
         {
-            throw new NotImplementedException();
+            _logger.LogDebug("Creating a new basket with identity {basketId}", basket.Id);
+
+            if (this.Baskets.ContainsKey(basket.Id))
+                return Task.FromResult(Result.Failure<Guid>($"Cannot insert basket with identity '{basket.Id}' as it already exists in the collection"));
+
+            if (this.Baskets.TryAdd(basket.Id, basket.ToDto()))
+                return Task.FromResult(Result.Success(basket.Id));
+
+            return Task.FromResult(Result.Failure<Guid>($"Erro while saving basket with identity {basket.Id}"));
+
+        }
+
+        public Task<Result<IEnumerable<Domain.Models.Basket>>> GetAll()
+        {
+            return Task.FromResult(Result.Success(this.Baskets.Values.ToDomain()));
         }
 
         public Task<Result<Domain.Models.Basket>> GetById(Guid id)
         {
-            throw new NotImplementedException();
+            _logger.LogDebug($"FindMany Basket with identity {id}");
+
+            if (!this.Baskets.ContainsKey(id))
+            {
+                return Task.FromResult(Result.Failure<Domain.Models.Basket>($"Basket with identity {id} not found!"));
+            }
+
+            _logger.LogDebug($"Found Basket with identity {id}");
+            return Task.FromResult(Result.Success(this.Baskets[id].ToDomain()));
         }
 
-        public Task<Result> Update(Domain.Models.Basket entity)
+        public Task<Result> Update(Domain.Models.Basket basket)
         {
-            throw new NotImplementedException();
+            if (!this.Baskets.ContainsKey(basket.Id))
+            {
+                return Task.FromResult(Result.Failure($"Cannot update basket with identity '{basket.Id}' as it doesn't exist"));
+            }
+
+            Baskets[basket.Id] = basket.ToDto();
+            _logger.LogDebug($"Basket with identity {basket.Id} updated.");
+
+            return Task.FromResult(Result.Success());
+        }
+
+        public Task<Result> Delete(Guid id)
+        {
+            if (!this.Baskets.ContainsKey(id))
+            {
+                return Task.FromResult(Result.Failure($"Cannot delete basket with identity '{id}' as it doesn't exist"));
+            }
+
+            return Task.FromResult(Result.SuccessIf(Baskets.Remove(id, out _), $"Error while deleting basket with identity {id}"));
+        }
+
+        public Task<Result<IEnumerable<Domain.Models.Basket>>> GetByFilter(Func<Domain.Models.Basket, bool> predicate)
+        {
+            var result = this.Baskets.Values.ToDomain().Where(predicate);
+
+            if (result.Any())
+                return Task.FromResult(Result.Success(result));
+
+            return Task.FromResult(Result.Failure<IEnumerable<Domain.Models.Basket>>("There are no baskets to the given predicate"));
         }
     }
 }
--- Impact.Basket.Api.Repository/InMemoryProductRepository.cs
+++ Impact.Basket.Api.Repository/InMemoryProductRepository.cs
@@ -3,6 +3,7 @@ using Impact.Basket.Api.Domain.Model.Filters;
 using Impact.Basket.Api.Domain.Models;
 using Impact.Basket.Api.Domain.Repositories.Contracts;
 using Impact.Basket.Api.Repository.Dtos;
+using Impact.Basket.Api.Repository.Mappers;
 using Microsoft.Extensions.Logging;
 using System.Collections.Concurrent;
 
@@ -18,34 +19,75 @@ namespace Impact.Basket.Api.Repository
             _logger = logger;
         }
 
-        public Task<Result<int>> Create(Product entity)
+        public Task<Result<int>> Create(Product product)
         {
-            throw new NotImplementedException();
-        }
+            _logger.LogDebug("Creating a new product with identity {productId}", product.Id);
 
+            if (this.Products.ContainsKey(product.Id))
+                return Task.FromResult(Result.Failure<int>($"Cannot insert product with identity '{product.Id}' as it already exists in the collection"));
+
+            if (this.Products.TryAdd(product.Id, product.ToDto()))
+                return Task.FromResult(Result.Success<int>(product.Id));
+
+            return Task.FromResult(Result.Failure<int>($"Erro while saving product with identity {product.Id}"));
+        }
         public Task<Result> Delete(int id)
         {
-            throw new NotImplementedException();
-        }
+            if (!this.Products.ContainsKey(id))
+            {
+                return Task.FromResult(Result.Failure($"Cannot delete product with identity '{id}' as it doesn't exist"));
+            }
 
+            return Task.FromResult(Result.SuccessIf(Products.Remove(id, out _), $"Error while deleting product with identity {id}"));
+
+        }
         public Task<Result<IEnumerable<Product>>> GetAll(PaginationFilter paginationFilter = null)
         {
-            throw new NotImplementedException();
-        }
+            if (paginationFilter is null)
+            {
+                _logger.LogDebug("Retrieving all products at once");
+                return Task.FromResult(Result.Success(this.Products.Values.ToDomain()));
+            }
 
-        public Task<Result<IEnumerable<Product>>> GetByFilter(Func<Product, bool> predicate)
-        {
-            throw new NotImplementedException();
+            _logger.LogDebug("Retrieving paged products with page number {pageNumber} and page size of {pageSize}", paginationFilter.PageNumber, paginationFilter.PageSize);
+
+            return Task.FromResult(Result.Success(this.Products.Values.Skip((paginationFilter.PageSize - 1 * paginationFilter.PageNumber)).Take(paginationFilter.PageSize).ToDomain()));
         }
 
         public Task<Result<Product>> GetById(int id)
         {
-            throw new NotImplementedException();
+            _logger.LogDebug($"FindMany Product with identity {id}");
+
+            if (!this.Products.ContainsKey(id))
+            {
+                return Task.FromResult(Result.Failure<Product>("Product not found!"));
+            }
+
+            _logger.LogDebug($"Found Product with identity {id}");
+            return Task.FromResult(Result.Success(this.Products[id].ToDomain()));
         }
 
-        public Task<Result> Update(Product entity)
+        public Task<Result> Update(Product product)
         {
-            throw new NotImplementedException();
+            if (!this.Products.ContainsKey(product.Id))
+            {
+                return Task.FromResult(Result.Failure($"Cannot update product with identity '{product.Id}' as it doesn't exist"));
+            }
+
+            Products[product.Id] = product.ToDto();
+            _logger.LogDebug($"product with identity {product.Id} updated.");
+
+            return Task.FromResult(Result.Success());
+        }
+
+        public Task<Result<IEnumerable<Product>>> GetByFilter(Func<Product, bool> predicate)
+        {
+            var result = this.Products.Values.ToDomain().Where(predicate);
+
+            if (result.Any())
+                return Task.FromResult(Result.Success(result));
+
+            return Task.FromResult(Result.Failure<IEnumerable<Product>>("There are no products to the given predicate"));
         }
     }
 }
