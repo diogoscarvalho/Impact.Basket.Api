a1e6bd1467003c2285d270fff5e0eb2d70359f33
Diogo <diogo.santos-carvalho@rabobank.com>
2023-07-17 07:47:43 +0200

    create unit tests with some domain model tests and integration test project skeleton

---
+++ Impact.Basket.Api.UnitTests/Domain/Models/BasketItemTests.cs
@@ -0,0 +1,79 @@
+﻿using FluentAssertions;
+using Impact.Basket.Api.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Impact.Basket.Api.UnitTests.Domain.Models
+{
+    [TestClass]
+    public class BasketItemTests
+    {
+        [TestMethod]
+        public void Constructor_InitializesProperties()
+        {
+            // Arrange
+            var product = new Product(1, "Product 1", 12.00M, 2, 3);
+            var quantity = 2;
+
+            // Act
+            var basketItem = new BasketItem(product, quantity);
+
+            // Assert
+            basketItem.Product.Should().Be(product);
+            basketItem.Quantity.Should().Be(quantity);
+            basketItem.TotalPrice.Should().Be(product.Price * quantity);
+        }
+
+        [TestMethod]
+        public void IncreaseQuantity_IncrementsQuantityAndAdjustsTotalPrice()
+        {
+            // Arrange
+            var product = new Product(1, "Product 1", 12.00M, 2, 3);
+            var quantity = 2;
+            var basketItem = new BasketItem(product, quantity);
+
+            // Act
+            basketItem.IncreaseQuantity(1);
+
+            // Assert
+            basketItem.Quantity.Should().Be(quantity + 1);
+            basketItem.TotalPrice.Should().Be(product.Price * (quantity + 1));
+        }
+
+        [TestMethod]
+        public void DecreaseQuantity_DecrementsQuantityAndAdjustsTotalPrice()
+        {
+            // Arrange
+            var product = new Product(1, "Product 1", 12.00M, 2, 3);
+            var quantity = 2;
+            var basketItem = new BasketItem(product, quantity);
+
+            // Act
+            basketItem.DecreaseQuantity();
+
+            // Assert
+            basketItem.Quantity.Should().Be(quantity - 1);
+            basketItem.TotalPrice.Should().Be(product.Price * (quantity - 1));
+        }
+
+        [TestMethod]
+        public void DecreaseQuantity_WithZeroQuantity_DoesNotChangeQuantityOrTotalPrice()
+        {
+            // Arrange
+            var product = new Product(1, "Product 1", 12.00M, 2, 3);
+            var quantity = 0;
+            var basketItem = new BasketItem(product, quantity);
+
+            // Act
+            basketItem.DecreaseQuantity();
+
+            // Assert
+            basketItem.Quantity.Should().Be(quantity);
+            basketItem.TotalPrice.Should().Be(product.Price * quantity);
+        }
+    }
+
+}
---
+++ Impact.Basket.Api.UnitTests/Domain/Models/BasketTests.cs
@@ -0,0 +1,75 @@
+﻿using FluentAssertions;
+using Impact.Basket.Api.Domain.Models;
+
+namespace Impact.Basket.Api.UnitTests.Domain.Models
+{
+    [TestClass]
+    public class BasketTests
+    {
+        [TestMethod]
+        public void AddItem_NewItem_AddsItemToBasket()
+        {
+            // Arrange
+            var basket = new Api.Domain.Models.Basket();
+            var item = new BasketItem(new Product(1, "Product 1", 45.00M, 3, 5), 1);
+
+            // Act
+            basket.AddItem(item);
+
+            // Assert
+            basket.Items.Should().ContainKey(item.Product.Id);
+            basket.Items[item.Product.Id].Should().Be(item);
+        }
+
+        [TestMethod]
+        public void AddItem_ExistingItem_IncreasesItemQuantity()
+        {
+            // Arrange
+            var basket = new Api.Domain.Models.Basket();
+            var product = new Product(1, "Product 1", 45.00M, 3, 5);
+            var item1 = new BasketItem(product, 1);
+            var item2 = new BasketItem(product, 1);
+
+            // Act
+            basket.AddItem(item1);
+            basket.AddItem(item2);
+
+            // Assert
+            basket.Items[product.Id].Quantity.Should().Be(2);
+        }
+
+        [TestMethod]
+        public void RemoveItem_ExistingItemWithQuantityOne_RemovesItemFromBasket()
+        {
+            // Arrange
+            var basket = new Api.Domain.Models.Basket();
+            var item = new BasketItem(new Product(1, "Product 1", 45.00M, 3, 5), 1);
+            basket.AddItem(item);
+
+            // Act
+            basket.RemoveItem(item);
+
+            // Assert
+            basket.Items.Should().NotContainKey(item.Product.Id);
+        }
+
+        [TestMethod]
+        public void RemoveItem_ExistingItemWithQuantityGreaterThanOne_DecreasesItemQuantity()
+        {
+            // Arrange
+            var basket = new Api.Domain.Models.Basket();
+            var product = new Product(1, "Product 1", 45.00M, 3, 5);
+            var item1 = new BasketItem(product, 1);
+            var item2 = new BasketItem(product, 2);
+            basket.AddItem(item1);
+            basket.AddItem(item2);
+
+            // Act
+            basket.RemoveItem(item1);
+
+            // Assert
+            basket.Items[product.Id].Quantity.Should().Be(2);
+        }
+    }
+
+}
