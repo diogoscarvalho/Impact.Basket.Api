3f13f7111438a34f67fb97f17c3430edd0e4df31
Diogo <diogo.santos-carvalho@rabobank.com>
2023-07-17 08:10:11 +0200

    fix class name and add contructor

--- Impact.Basket.Api.Repository/InMemoryBasketRepository.cs
+++ Impact.Basket.Api.Repository/InMemoryBasketRepository.cs
@@ -1,11 +1,22 @@
 ﻿using CSharpFunctionalExtensions;
 using Impact.Basket.Api.Domain.Model.Filters;
 using Impact.Basket.Api.Domain.Repositories.Contracts;
+using Impact.Basket.Api.Repository.Dtos;
+using Microsoft.Extensions.Logging;
+using System.Collections.Concurrent;
 
 namespace Impact.Basket.Api.Repository
 {
     public class InMemoryBasketRepository : IGenericRepository<Domain.Models.Basket, Guid>
     {
+        private ConcurrentDictionary<Guid, BasketDto> Baskets { get; } = new ConcurrentDictionary<Guid, BasketDto>();
+        private ILogger<InMemoryBasketRepository> _logger;
+
+        public InMemoryBasketRepository(ILogger<InMemoryBasketRepository> logger)
+        {
+            _logger = logger;
+        }
+
         public Task<Result<Guid>> Create(Domain.Models.Basket entity)
         {
             throw new NotImplementedException();
---
+++ Impact.Basket.Api.Repository/InMemoryProductRepository.cs
@@ -0,0 +1,51 @@
+﻿using CSharpFunctionalExtensions;
+using Impact.Basket.Api.Domain.Model.Filters;
+using Impact.Basket.Api.Domain.Models;
+using Impact.Basket.Api.Domain.Repositories.Contracts;
+using Impact.Basket.Api.Repository.Dtos;
+using Microsoft.Extensions.Logging;
+using System.Collections.Concurrent;
+
+namespace Impact.Basket.Api.Repository
+{
+    public class InMemoryProductRepository : IGenericRepository<Product, int>
+    {
+        private ConcurrentDictionary<int, ProductDto> Products { get; } = new ConcurrentDictionary<int, ProductDto>();
+        private ILogger<InMemoryProductRepository> _logger;
+
+        public InMemoryProductRepository(ILogger<InMemoryProductRepository> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task<Result<int>> Create(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<IEnumerable<Product>>> GetAll(PaginationFilter paginationFilter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<IEnumerable<Product>>> GetByFilter(Func<Product, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result<Product>> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Result> Update(Product entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
